70c54faf9432f2b7c4f58b200a1ba5ff4f845b0c
Alejandro Henao <johan.alejo.1099@gmail.com>
2018-05-02 22:33:07 -0500

    Some changes

---
+++ Find the couples/Assets/Prueba.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+public class Prueba : MonoBehaviour 
+{
+	public struct OperationJob
+	{
+
+	}
+}
